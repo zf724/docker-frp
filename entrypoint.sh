c31edb33f1857853ad18b086add201e7f8d93382
zhangfeng <zf724@qq.com>
2017-07-02 16:07:12 +0800

    need privilege token to connect server,open log record

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,21 @@
 if [ ${IS_SERVER} == true ];then
   { \
        echo "[common]"; \
+       echo "bind_addr = 0.0.0.0"; \
        echo "bind_port = ${BIND_PORT}"; \
-       echo "subdomain_host =  ${SUBDOMAIN_HOST}"; \
        echo "vhost_http_port = ${VHOST_HTTP_PORT}"; \
        echo "vhost_https_port = ${VHOST_HTTPS_PORT}"; \
-       echo "max_pool_count = ${MAX_POOL_COUNT}"; \
        echo "dashboard_port = ${DASHBOARD_PORT}"; \
        echo "dashboard_user = ${DASHBOARD_USER}"; \
        echo "dashboard_pwd = ${DASHBOARD_PWD}"; \
+       echo "log_file = ${LOG_FILE}"; \
+       echo "log_level = ${LOG_LEVEL}"; \
+       echo "log_max_days = ${LOG_MAX_DAYS}"; \
+       echo "privilege_token = ${PRIVILEGE_TOKEN}"; \
+       echo "max_pool_count = ${MAX_POOL_COUNT}"; \
+       echo "authentication_timeout = ${AUTHENTICATION_TIMEOUT}"; \
+       echo "subdomain_host =  ${SUBDOMAIN_HOST}"; \
+       echo "tcp_mux = true"; \
   } > frps.ini
 
   mv frp_${VERSION}_linux_amd64/frps .
@@ -21,8 +28,13 @@ else
   { \
        echo "[common]"; \
        echo "server_addr = ${SERVER_ADDR}"; \
-       echo "bind_port = ${BIND_PORT}"; \
+       echo "server_port = ${BIND_PORT}"; \
+       echo "log_file = ${LOG_FILE}"; \
+       echo "log_level = ${LOG_LEVEL}"; \
+       echo "log_max_days = ${LOG_MAX_DAYS}"; \
+       echo "privilege_token = ${PRIVILEGE_TOKEN}"; \
        echo "pool_count = ${POOL_COUNT}"; \
+       echo "tcp_mux = true"; \
        echo "[ssh]"; \
        echo "type = tcp"; \
        echo "local_ip = ${SSH_LOCAL_IP}"; \
